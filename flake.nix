5b40aad4becf875641c5a52c60fc673a5158ae84
jupblb <michal.kielbowicz@sourcegraph.com>
2025-08-17 10:12:53 +0200

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url     = "github:NixOS/nixpkgs/release-25.05";
+  };
+
+  outputs = { self, flake-utils, nixpkgs }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = (import nixpkgs) { inherit system; };
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [ pandoc pre-commit ];
+        };
+      }
+    );
+}
